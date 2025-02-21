7ce4a8c2f48a5ce8948a1c335094b3256c81f3c0
breno-ca <breno_de_avila@outlook.com>
2025-02-21 12:52:20 -0300

    feat: added public home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PublicComponent } from './public/public.component';
+import { authGuard } from './auth/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PublicComponent
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),
+    canActivate: [authGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
